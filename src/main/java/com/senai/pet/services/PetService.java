9fe2913970037f9149c3cbf5e7de1df1c1396a20
srjuninn <ryaferreiradasilva2006@gmail.com>
2026-01-28 10:50:25 -0300

    feat: add save pet method

--- src/main/java/com/senai/pet/services/PetService.java
+++ src/main/java/com/senai/pet/services/PetService.java
@@ -1,6 +1,7 @@
 package com.senai.pet.services;
 
 import com.senai.pet.dtos.PetDto;
+import com.senai.pet.entities.PetEntity;
 import com.senai.pet.repositories.PetRepository;
 import org.springframework.stereotype.Service;
 
@@ -9,9 +10,13 @@ public class PetService {
 
     private PetRepository petRepository;
 
-    public PetService(PetRepository petRepository){
+    public PetService(PetRepository petRepository) {
         this.petRepository = petRepository;
     }
 
-
+    public String salvar(PetDto dto) {
+        PetEntity pet = new PetEntity(dto.getNome(), dto.getIdade(), dto.getRaca(), dto.getPorte(), dto.getTipo());
+        petRepository.save(pet);
+        return "salvo com sucesso!";
+    }
 }
